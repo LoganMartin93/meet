20ef73c24af2189320823a7de2f55774f5c748d5
LoganMartin93 <166079175+LoganMartin93@users.noreply.github.com>
2024-12-11 11:24:38 -0800

    what is wrong with my manifest file?

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
-import CityEventsChart from './components/CityEventsChart';
 import EventList from './components/EventList';
 import CitySearch from './components/CitySearch';
 import NumberOfEvents from './components/NumberOfEvents';
+import CityEventsChart from './components/CityEventsChart';
 import { useEffect, useState } from 'react';
 import { extractLocations, getEvents } from './api';
 import { InfoAlert, ErrorAlert, WarningAlert} from './components/Alert';
